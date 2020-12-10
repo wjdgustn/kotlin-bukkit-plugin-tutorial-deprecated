faf9be9c80cc9830b4068423cd5008851d1b057f
Hyunsoo, Chung <admin@hyonsu.com>
2020-12-10 11:31:52 +0900

    Initial commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const branch = 'master';
 
 window.onload = () => {
-    if(!window.location.hostname.endsWith('.github.io')) return throw new Error('Here is not github.io website.');
+    if(!window.location.hostname.endsWith('.github.io')) throw new Error('Here is not github.io website.');
     const repo = `${window.location.hostname.replace('.github.io', '')}/${window.location.pathname == '/' ? window.location.hostname : (window.location.pathname.replace('/', '').split('/')[0])}`;
 
     let page = 1;
